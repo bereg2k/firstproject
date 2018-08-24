e5b4d2baebc0bf15fd2ed346012226e39b929ffe
bereg2k <bereg2k@gmail.com>
2018-08-24 20:19:29 +0300

    v3

--- src/main/java/Base.java
+++ src/main/java/Base.java
@@ -2,6 +2,7 @@ public class Base
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello, world! Hello!!!");
+        System.out.println("Hello, world!");
+        System.out.println("How u doin?");
     }
 }
