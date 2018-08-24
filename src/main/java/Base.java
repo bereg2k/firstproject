8bf2a4ac7232234c93772403614afb8c90b83311
bereg2k <bereg2k@gmail.com>
2018-08-24 23:55:28 +0300

    v4

--- src/main/java/Base.java
+++ src/main/java/Base.java
@@ -5,5 +5,6 @@ public class Base
         System.out.println("Hello, world!");
         System.out.println("How u doin?");
         System.out.println("I'm fine thanks");
+        System.out.println("And you?");
     }
 }
